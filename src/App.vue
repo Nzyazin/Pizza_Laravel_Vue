d1c207d0fe2347fd9517d8f84d93b9de883dbb6c
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2022-11-24 00:46:02 +0300

    change front

--- src/App.vue
+++ src/App.vue
@@ -43,7 +43,7 @@
                 <div class="d-lg-block d-none">
                     <div class="row g-0 position-relative">
                         <div class="col-lg-3 d-flex align-items-center justify-content-center border-rit ">
-                            <div class="logo"> <a href="index.html"> <img src="src/assets/images/logo/logo.png" alt=""> </a>
+                            <div class="logo"> <a href="index.html"> <img src="src/assets/images/logo/logo.png" width="120" height="120" alt=""> </a>
                             </div>
                         </div>
                         <div class="col-lg-9 g-0 p-0">
@@ -51,19 +51,8 @@
                                 <div class="col-12">
                                     <div class="some-info">
                                         <p class="d-flex align-items-center"> <span class="icon"> <i
-                                                    class="flaticon-power"></i> </span> Welcome to Karte Online Shop</p>
-                                        <div class="right d-flex align-items-center ">
-                                            <div class="language currency"> <select>
-                                                    <option>USD</option>
-                                                    <option value="1">INR</option>
-                                                    <option value="2">BDT</option>
-                                                </select> </div>
-                                            <div class="language two"> <select>
-                                                    <option>ENGLISH </option>
-                                                    <option value="1">GERMAN</option>
-                                                    <option value="4">FRENCH</option>
-                                                </select> </div> <a href="login.html"> Sign In / Register </a>
-                                        </div>
+                                                    class="flaticon-power"></i> </span> Добро пожаловать в магазин</p>
+
                                     </div>
                                 </div>
                             </div>
@@ -79,15 +68,11 @@
                                                   <li class="dropdown-list"> <router-link to="/pizza"> <span>Заказать </span> </router-link></li>
                                                 </ul>
                                             </nav>
- 
+
 
                                             <div class="right d-flex align-items-center justify-content-end">
                                                 <ul class="main-menu__widge-box d-flex align-items-center ">
-                                                    <li class="d-lg-block d-none"><a href="my-account.html"><i
-                                                                class="flaticon-user"></i> </a></li>
-                                                    <li class="d-lg-block d-none"><a href="wishlist.html"
-                                                            class="number"><i class="flaticon-heart"></i> <span
-                                                                class="count">(2)</span> </a> </li>
+
                                                     <li class="cartm"> <a href="#0" class="number cart-icon"> <i
                                                                 class="flaticon-shopping-cart"></i><span
                                                                 class="count">(5)</span> </a> </li>
@@ -219,64 +204,42 @@
                         <div class="footer-default__single-box">
                             <div class="company-info">
                                 <div class="footer-title">
-                                    <h4> Office</h4>
+                                    <h4> Офис</h4>
                                 </div>
                                 <div class="text1">
-                                    <p>29 Holles Place, Dublin 2 D02 YY46</p>
+                                    <p>Киров</p>
                                 </div>
                                 <div class="text2">
-                                    <p>68 Jay Street, Suite 902 New Side <br> Brooklyn, NY 11201</p>
+                                    <p>Москва</p>
                                 </div>
                                 <div class="text3">
-                                    <p>New York, United States</p>
+                                    <p>Пермь</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-xl-2 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
-                        <div class="footer-default__single-box">
-                            <div class="footer-title">
-                                <h4> Useful Links </h4>
-                            </div>
-                            <ul class="footer-links">
-                                <li><a href="my-account.html">Account</a></li>
-                                <li><a href="login.html">Sign In</a></li>
-                                <li><a href="cart.html">View Cart</a></li>
-                                <li><a href="wishlist.html">My WishList</a></li>
-                                <li><a href="compare.html">Compare Products</a></li>
-                            </ul>
-                        </div>
-                    </div>
+
                     <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
                         <div class="footer-default__single-box">
                             <div class="footer-title">
-                                <h4> Information </h4>
+                                <h4> Информация </h4>
                             </div>
                             <ul class="footer-links">
-                                <li><a href="about-us.html">About us</a></li>
-                                <li><a href="contact.html">Contact Us </a></li>
-                                <li><a href="faq.html">Faq</a></li>
-                                <li><a href="blog.html">Latest Posts</a></li>
-                                <li><a href="order-track.html">Order Track</a></li>
+                                <li><a href="about-us.html">О нас</a></li>
+                                <li><a href="contact.html">Контакты </a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-xl-4 col-lg-6 col-md-6 col-sm-12 mt-30 wow fadeInUp animated">
                         <div class="footer-default__single-box">
                             <div class="footer-title">
-                                <h4> Newsletter </h4>
+                                <h4> Соц сети </h4>
                             </div>
                             <div class="footer-newsletter">
-                                <p class="text">Enter your email to receive our latest updates about our products &
-                                    promotions. </p>
-                                <form action="#0" class="footer-default__subscrib-form">
-                                    <div class="footer-input-box"> <input type="email" placeholder="Email address"
-                                            name="email"> <button type="submit" class="subscribe_btn"> Subscribe
-                                        </button> </div>
-                                </form>
+
                                 <div class="newsletter-bottom d-flex align-items-center">
                                     <div class="footer-title-box">
-                                        <p>Follow Us:</p>
+                                        <p>Следуйте за нами:</p>
                                     </div>
                                     <div class="footer__medio-boxx  medio-boxx  d-flex align-items-center">
                                         <ul>
@@ -301,7 +264,7 @@
             <div class="container">
                 <div class="footer_bottom_content">
                     <div class="copyright wow fadeInUp animated">
-                        <p>© 2022 <a href="index.html">Karte.</a> All Rights Reserved.</p>
+                        <p>© 2022 <a href="index.html">Магазин.</a> Все права защищены.</p>
                     </div>
                     <div class="footer-payment wow fadeInUp animated">
                         <a href="#0"> <img src="src/assets/images/home-four/method-1.jpg" alt="payment"> </a>
--- src/views/pizza/Index.vue
+++ src/views/pizza/Index.vue
@@ -1,32 +1,10 @@
 <template>
   <div>
     <main class="overflow-hidden ">
+
         <!--Start product-grid-->
         <section class="product-grid pt-60 pb-120">
             <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="shop-grid-page-top-info justify-content-md-between justify-content-center">
-                            <div class="left-box wow fadeInUp animated">
-                                <p>Showing 1–12 of 50 Results</p>
-                            </div>
-                            <div
-                                class="right-box justify-content-md-between justify-content-center wow fadeInUp animated">
-                                <div class="short-by">
-                                    <div class="select-box"> <select class="wide">
-                                        <option data-display="Short by latest">Featured </option>
-                                        <option value="1">Best selling </option>
-                                        <option value="2">Alphabetically, A-Z</option>
-                                        <option value="3">Alphabetically, Z-A</option>
-                                        <option value="3">Price, low to high</option>
-                                        <option value="3">Price, high to low</option>
-                                        <option value="3">Date, old to new</option>
-                                    </select> </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 <div class="row">
                     <div v-for="product in products" class="col-xl-3 col-lg-4 col-6 wow fadeInUp animated">
                         <div class="products-three-single w-100 wow fadeInUp animated mt-30">
@@ -114,21 +92,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-12 d-flex justify-content-center wow fadeInUp animated">
-                        <ul class="pagination text-center">
-                            <li class="next"><a href="#0"><i class="flaticon-left-arrows" aria-hidden="true"></i> </a>
-                            </li>
-                            <li><a href="#0">1</a></li>
-                            <li><a href="#0" class="active">2</a></li>
-                            <li><a href="#0">3</a></li>
-                            <li><a href="#0">...</a></li>
-                            <li><a href="#0">10</a></li>
-                            <li class="next"><a href="#0"><i class="flaticon-next-1" aria-hidden="true"></i> </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+
             </div>
         </section>
         <!--End product-grid-->
