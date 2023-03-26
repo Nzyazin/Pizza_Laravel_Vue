2c001570602e422729f274dbc102141f06274a30
Nzyazin <69486560+Nzyazin@users.noreply.github.com>
2023-03-26 10:24:54 +0300

    creating product model - 'pizzas'

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $table = 'pizzas';
+    protected $guarded = false;
+
+    public function getImageUrlAttribute() {
+        return url('storage/' . $this->preview_image);
+    }
+    
     use HasFactory;
 }
